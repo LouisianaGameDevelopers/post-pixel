d8a2cec35ea284806a55de028cf3f8915b454126
Nicholas Scheurich <nick@scheurich.me>
2018-06-05 17:00:21 -0500

    Add CSS properties order config

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,44 @@
 module.exports = {
-  extends: 'stylelint-config-standard'
+  extends: 'stylelint-config-standard',
+  plugins: ['stylelint-order'],
+  rules: {
+    'order/order': ['custom-properties', 'declarations'],
+    'order/properties-order': [
+      'display',
+      'width',
+      'height',
+      'margin',
+      'padding',
+
+      'position',
+      'z-index',
+      'top',
+      'right',
+      'bottom',
+      'left',
+
+      'font',
+      'font-weight',
+      'font-size',
+      'line-height',
+      'font-family',
+      'color',
+
+      'border',
+      'border-top',
+      'border-right',
+      'border-bottom',
+      'border-left',
+
+      'background',
+      'background-size',
+      'background-position',
+      'background-repeat',
+      'background-color',
+      'background-image',
+
+      'transform',
+      'opacity'
+    ]
+  }
 };
